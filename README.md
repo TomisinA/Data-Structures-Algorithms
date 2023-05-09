188c221a157413e442a10c442a59b824f3beac48
Tomisin Awosika <tomisinawosika@gmail.com>
2023-05-09 20:48:39 +0100

    Binary Search Iterative Method

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Data-Structures-Algorithms
-This repository contains different data structures and algorithms, how to program them and sample solved questions 
+This repository contains different data structures and algorithms, how to program them and contains some sample solved questions
---
+++ Searching/binary_search_iter.py
@@ -0,0 +1,20 @@
+#BINARY SEARCH (ITERATIVE METHOD)
+def binarySearch(num2, x):
+  low = 0 #define variable for starting point
+  high = len(num2) - 1 #define variable for ending point
+
+  while low < high: #start a while loop that checks for the mid using floor division to return a whole number
+    mid = (high + low) // 2
+
+    if x == num2[mid]: #if true immediately return the found index
+      return mid
+    
+    elif x > num2[mid]: #if true make the low after the mid by 1 and then check condition again
+      low = mid + 1
+
+    else:
+      high = mid - 1 #if true make the high below the mid by 1 and then check condition again
+
+  return -1 #do this if the while loop condition above isnt true
+
+binarySearch([10,20,30,40,50,60,70], 5)
